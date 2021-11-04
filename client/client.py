2e04db48624da88bdf25b693d247092f60f2d23e
sanix <sanicksikani@gmail.com>
2021-11-04 15:15:17 +0300

    time for streaming

--- client/client.py
+++ client/client.py
@@ -3,8 +3,12 @@ import threading
 import time
 
 import matplotlib
+import matplotlib.axes
 from PyQt5.QtCore import QObject, pyqtSignal
+from matplotlib import animation
 from matplotlib.animation import TimedAnimation
+import matplotlib; matplotlib.use("TkAgg")
+
 
 matplotlib.use('Qt5Agg')
 
@@ -19,7 +23,7 @@ import grpc
 import protofiles_pb2
 import protofiles_pb2_grpc
 
-""""
+"""
 def run():
     channel = grpc.insecure_channel("localhost:5000")
     try:
@@ -36,18 +40,21 @@ def run():
     request.y.extend(y.tolist())
 
     parts=stub.compute(request)
-    for x in range(0, 3):
+    for x in parts:
+
         print('Data Received from server ', x)
-        a=next(parts)
-        print(a.z[0])
+        a=x
+        print(dir(a))
+        #a=next(parts)
+        #print(a.z[0])
        
 
-run()
+myDataLoop = threading.Thread(name='myDataLoo', target=run, daemon=True,
+                                      args=())
+myDataLoop.start()
+
+myDataLoop.join()
 """
-
-
-
-
 
 
 
@@ -55,88 +62,68 @@ class Communicate(QObject):
     data_signal = pyqtSignal(float)
 
 
+class MplCanvas(FigureCanvasQTAgg):
 
+    def update(self):
+        # z = np.sin(x+y+i)
+        print('called')
+       # time.sleep(2)
+        self.z = np.cos(self.X) * np.sin(self.Y)
+        self.ax.clear()
+        self.line = self.ax.plot_surface(self.X, self.Y, self.z, color='b')
+        return self.line,
 
+    def __init__(self):
 
-class MplCanvas(FigureCanvasQTAgg, TimedAnimation):
-
-    def __init__(self, parent=None):
-        fig = plt.figure()
-        self.ax = plt.axes(projection="3d")
-        super(MplCanvas, self).__init__(fig)
+        # n = 2.*np.pi
+        self.fig = plt.figure()
+        #self.ax = fig.add_subplot(111, projection='3d')
+        self.ax=plt.axes(projection="3d")
+        super(MplCanvas, self).__init__(self.fig)
         self.x = np.linspace(-6, 6, 120)
         self.y = np.linspace(-6, 6, 120)
-        self.Z = []
-        myDataLoop = threading.Thread(name='myDataLoop', target=self.receiveData, daemon=True,
-                                      args=())
-        myDataLoop.start()
-        TimedAnimation.__init__(self, fig, interval=50, blit=True)
+        self.X, self.Y = np.meshgrid(self.x, self.y)
+        self.Z = np.sin(self.X) * np.cos(self.Y)
+        self.line = self.ax.plot_surface(self.X[:60], self.Y[:60], self.Z[:60], color='b')
+
+        self.ani = animation.FuncAnimation(self.fig, self.update, fargs=(self.Z, self.line), interval=10, blit=False)
+        plt.ion
+        #dataThread=threading.Thread(name='mydata', target=self.update, daemon=True)
+        #dataThread.start()
+
+        #plt.show()
 
 
-    def new_frame_seq(self):
-        return iter(range(self.x.size))
-
-    def plotit(self):
-        time.sleep(4)
-        self.X, self.Y = np.meshgrid(self.x, self.y)#be removed
-        #self.Z=np.cos(self.X)*np.sin(self.Y)
-        self.ax.plot_surface(self.X, self.Y, np.array(self.Z), rstride=1, cstride=1,
-                             cmap='winter', edgecolor='none')
-        self.ax.set_title('surface')
-
-
-    def _step(self, *args):
-        # Extends the _step() method for the TimedAnimation class.
-        try:
-            TimedAnimation._step(self, *args)
-        except Exception as e:
-            #self.abc += 1
-            #print(str(self.abc))
-            TimedAnimation._stop(self)
-            pass
-        return
-
-    def receiveData(self):
-        print('rece')
-        self.run()
-
-    #def addData_callbackFunc(self):
-    #    for x in next(run()):
-    #        i = [i for i in x.z]
-    #        self.Z.extend([i])
-    #    return
-
-    def run(self):
-        channel = grpc.insecure_channel("localhost:5000")
+    #def run(self):
         #mySrc = Communicate()
-        #mySrc.data_signal.connect(addData_callbackFunc)
-        try:
-            grpc.channel_ready_future(channel).result(timeout=10)
-        except grpc.FutureTimeoutError:
-            sys.exit('Error connecting to server')
-        stub = protofiles_pb2_grpc.ComputeFunctionStub(channel)
+        #mySrc.data_signal.connect(callBack)
+     #   channel = grpc.insecure_channel("localhost:5000")
+    #    try:
+     #       grpc.channel_ready_future(channel).result(timeout=10)
+     #   except grpc.FutureTimeoutError:
+      #      sys.exit('Error connecting to server')
+      #  stub = protofiles_pb2_grpc.ComputeFunctionStub(channel)
 
-        request = protofiles_pb2.DataRequest()
-        request.x.extend(self.x.tolist())
-        request.y.extend(self.y.tolist())
-        a=stub.compute(request)
-        print(dir(a))
-        self.Z=[]
-        for x in next(a).z:
-            i=[i for i in x.z]
-            self.Z.extend([i])
-        #print(self.Z)
+    #    request = protofiles_pb2.DataRequest()
+    #    request.x.extend(self.x.tolist())
+    #    request.y.extend(self.y.tolist())
+    #    self.a=stub.compute(request)
+        #for part in self.a:
+        #    self.Z=[]
+        #    for x in part.z:
+        #        i=[i for i in x.z]
+        #        self.Z.extend([i])
 
 
 class MainWindow(QtWidgets.QMainWindow):
 
     def __init__(self, *args, **kwargs):
         super(MainWindow, self).__init__(*args, **kwargs)
-        sc = MplCanvas(self)
+        sc = MplCanvas()
         self.setWindowTitle("my first window")
         layout = QtWidgets.QVBoxLayout()
         layout.addWidget(sc)
-        sc.plotit()
+        #sc.plotit()
         widget = QtWidgets.QWidget()
         widget.setLayout(layout)
         self.setCentralWidget(widget)
